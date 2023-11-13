e2e62dc893ce3684c2a0ca976485b345c4965eb2
Bhavesh Mankar <bmmankar25@gmail.com>
2023-11-13 21:25:00 +0530

    home pages changes made

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,34 +1,70 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { Button } from "@material-tailwind/react";
 import Skills from '../Pages/Skills';
 import Blogs from '../Pages/Blogs';
-
-
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <div className="container min-h-screen flex min-w-full flex-col  ">
+    <div className="container min-h-screen flex min-w-full flex-col">
       <main className="main flex-grow flex flex-col items-center justify-center bg-[#FFFFFF] p-5 rounded">
-        <div className="title font-bold text-6xl text-center">
+        <div className="title font-bold text-4xl md:text-6xl text-center">
           Hi!👋🏻 I am <br />
           <span className="text-[#3282B8]">Bhavesh Mankar</span>
         </div>
-        <p className="description text-center text-3xl my-4">
+        <p className="description text-center text-xl md:text-3xl my-4">
           A Full Stack developer and Problem Solver
         </p>
-        <div className="flex flex-row space-x-7 mt-4">
-          <Link to='/Project' className=" bg-blue-100 hover:bg-blue-200 text-blue-900 hover:text-blue-800 font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-blue-400">  <span>Projects</span></Link>
-          <Link to='/Blog' className="bg-blue-100 hover:bg-blue-200 text-blue-900 hover:text-blue-800 font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-blue-400" element={<Blogs/>}>Blogs</Link>
-          <Link to='/Skills' className="bg-blue-100 hover:bg-blue-200 text-blue-900 hover:text-blue-800 font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-blue-400y" element={<Skills />}>Skills</Link>
-          <Link to='/About' className="bg-blue-100 hover:bg-blue-200 text-blue-900 hover:text-blue-800 font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-blue-400">About Me</Link>
+        <div className="flex w-max items-end gap-4">
+          <Button  onClick={() => navigate('/Project')} variant="outlined" size="sm">Projects</Button>
+          <Button  onClick={() => navigate('/blog')} variant="outlined" size="sm">Blogs</Button>
+          <Button  onClick={() => navigate('/Skills')} variant="outlined" size="sm">Skills</Button>
+          <Button  onClick={() => navigate('/about')} variant="outlined" size="sm">About</Button>
         </div>
+       
+
         {/* For Bottom Buttons */}
-        <div className="flex flex-row space-x-4 mt-4">
-          <p className=" bg-gray-200 hover:bg-gray-300 text-gray-700 hover:text-gray-800 font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-gray-400"><a target="_blank" rel='noreferrer' href={""} >Resume</a></p>
-          <a target="_blank" rel='noreferrer' href={"https://github.com/bhavesh-03"} className="bg-gray-200 hover:bg-gray-300 text-gray-700 hover:text-gray-800 font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-gray-400">GitHub</a>
-          {/* Linkedin  */}
-          <a target="_blank" rel='noreferrer' href={"https://www.linkedin.com/in/bhavesh-mankar-7420ba22a/"} className="bg-gray-200 hover:bg-gray-300 text-gray-700 hover:text-gray-800 font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-gray-400">LinkedIn</a>
-          <a target="_blank" rel='noreferrer' href={"mailto:bmmankar25@gmail.com@gmail.com"} className="bg-gray-200 hover:bg-gray-300 text-gray-700 hover:text-gray-800 font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-gray-400">Email</a>
+        <div className="flex flex-col md:flex-row md:space-x-4 mt-6 space-x-4 justify-center items-center flex-wrap">
+          {/* Buttons for Resume and GitHub */}
+          <div className="space-x-4 mb-2 md:mb-0 flex">
+            <a
+              target="_blank"
+              rel='noreferrer'
+              href={""}
+              className="btn btn-bottom bg-blue-500 text-white px-4 py-2 rounded-full transition duration-300 ease-in-out hover:bg-blue-400"
+            >
+              Resume
+            </a>
+            <a
+              target="_blank"
+              rel='noreferrer'
+              href={"https://github.com/bhavesh-03"}
+              className="btn btn-bottom bg-blue-500 text-white px-4 py-2 rounded-full transition duration-300 ease-in-out hover:bg-blue-400"
+            >
+              GitHub
+            </a>
+          </div>
+          {/* LinkedIn and Email Buttons */}
+          <div className="space-x-4 flex">
+            <a
+              target="_blank"
+              rel='noreferrer'
+              href={"https://www.linkedin.com/in/bhavesh-mankar-7420ba22a/"}
+              className="btn btn-bottom bg-blue-500 text-white px-4 py-2 rounded-full transition duration-300 ease-in-out hover:bg-blue-400"
+            > 
+              LinkedIn
+            </a>
+            <a
+              target="_blank"
+              rel='noreferrer'
+              href={"mailto:bmmankar25@gmail.com@gmail.com"}
+              className="btn btn-bottom bg-blue-500 text-white px-4 py-2 rounded-full transition duration-300 ease-in-out hover:bg-blue-400"
+            >
+              Email
+            </a>
+          </div>
         </div>
       </main>
       <footer className="footer text-center border-t-2 border-t-neutral-200 bg-[#FAFAFA] text-gray-600 py-4 font-semibold">
